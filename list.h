6117f565da74d66337669578ae7b3c9dbb741576
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2018-08-21 21:53:37 -0500

    Implementacion template

--- list.h
+++ list.h
@@ -19,12 +19,49 @@ class List {
     public:
         List();
 
-        int front();
-        int back();
-        void push_front(T value);
-        void push_back(T value);
-        void pop_front();
-        void pop_back();
+        T front(){
+            return head -> data;
+        };
+        T back(){
+            return tail -> data;
+        };
+        void push_front(T value){
+            Node<T>* temp = new Node<T>;
+            temp -> data = value;
+            if (empty()){
+                temp -> next = nullptr;
+                tail = temp;
+                head = temp;
+            }
+            else{
+                head -> next = head;
+                head = temp;
+            }
+        };
+        void push_back(T value){
+            Node<T>* temp = new Node<T>;
+            temp -> data = value;
+            temp -> next = nullptr;
+            if (empty()){
+                tail = temp;
+                head = temp;
+            }
+            else{
+                tail -> next = temp;
+                tail = temp;
+            }
+        };
+        void pop_front(){
+            Node<T>* temp = head;
+            head -> next = head;
+            delete temp;
+        };
+        void pop_back(){
+            Node<T>* temp = tail;
+            //
+            tail -> next = nullptr;
+            delete temp;
+        };
         T get(int position);
         void concat(List<T> &other);
         bool empty();
