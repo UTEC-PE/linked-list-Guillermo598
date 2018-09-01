a51afd2caba731d46459ebbd5183651c5ac17fd9
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2018-08-31 20:33:02 -0500

    Correciones Pt. 1

--- list.h
+++ list.h
@@ -17,86 +17,92 @@ class List {
         void print_reverse(Node<T>* head);
 
     public:
-        List();
+        List(){
+            head = tail = nullptr;
+        };
 
         T front(){
+            if (head == nullptr)
+                throw "ListError";
             return head -> data;
         };
         T back(){
+            if (head == nullptr)
+                throw "ListError";
             return tail -> data;
         };
         void push_front(T value){
             Node<T>* temp = new Node<T>;
             temp -> data = value;
-            if (empty()){
-                temp -> next = nullptr;
-                tail = temp;
+            temp -> next = head;
+            if (empty())
+                head = tail = temp;
+            else
                 head = temp;
-            }
-            else{
-                head -> next = head;
-                head = temp;
-            }
         };
         void push_back(T value){
             Node<T>* temp = new Node<T>;
             temp -> data = value;
             temp -> next = nullptr;
-            if (empty()){
-                tail = temp;
-                head = temp;
-            }
-            else{
-                tail -> next = temp;
+            if (empty())
+                head = tail = temp;
+            else {
+                tail->next = temp;
                 tail = temp;
             }
         };
         void pop_front(){
-            Node<T>* temp = head;
-            head -> next = head;
-            delete temp;
+            if (empty())
+                throw "ListError";
+            else {
+                Node<T> *temp = head;
+                head = head->next;
+                delete temp;
+            }
         };
         void pop_back(){
-            Iterator = head;
-            while (Iterator -> next -> next != nullptr)
-                Iterator = Iterator -> next;
-            tail = Iterator;
-            tail -> next = nullptr;
+            if (empty())
+                throw "ListError";
+            else {
+                Node<T> *temp = head;
+                for (int i = 1; i < size()-1; i++)
+                    temp = temp -> next;
+                tail = temp;
+                temp = temp -> next;
+                delete temp;
+            }
         };
         T get(int position){
-            int i = 0;
-            Iterator = head;
-            while (Iterator != nullptr && i < position){
-                Iterator = Iterator -> next;
-                i++;
+            if (position > size()-1 || position < 0)
+                throw "ListError";
+            else{
+                Node<T>* temp = head;
+                for (int i = 0; i < position; i++)
+                    temp = temp -> next;
+                return temp -> data;
             }
-            T value = Iterator -> data;
-            return value;
         };
         void concat(List<T> &other){
             this -> tail -> next = other -> head;
             this -> tail = other -> tail;
         };
         bool empty(){
-            if (size() == 0)
-                return true;
-            else
-                return false;
+            return head == nullptr;
         };
         int size(){
-            Iterator = head;
+            Node<T>* temp = head;
             int size = 0;
-            while (Iterator != nullptr){
-                Iterator = Iterator -> next;
+            while (temp != nullptr){
+                temp = temp -> next;
                 size++;
             }
             return size;
         };
         void print(){
-            Iterator = head;
-            while (Iterator != nullptr){
-                cout << Iterator -> data << endl;
-                Iterator = Iterator -> next;
+            Node<T>* temp = head;
+            while (temp != nullptr){
+                cout << temp -> data << endl;
+                temp = temp -> next;
             }
         };
         void print_reverse(){
@@ -113,6 +119,8 @@ class List {
         Iterator<T> begin();
         Iterator<T> end();
 
-        ~List();
+        ~List(){
+            head -> killSelf();
+        };
 };
 #endif
